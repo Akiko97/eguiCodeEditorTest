2f820678585503d36649676e94fc610a56bc7283
Gabriel Zhong <mud.miscue_0l@icloud.com>
2023-11-14 19:39:17 +0900

    test asm syntax

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,20 @@ struct MyApp {
 impl Default for MyApp {
     fn default() -> Self {
         Self {
-            code: "// A very simple example\n\
-fn main() {\n\
-\tprintln!(\"Hello world!\");\n\
-}\n\
-".into(),
+            code: "; Test
+vextractf128 xmm0, ymm0, 1
+vbroadcastsd ymm5, xmm0
+shufpd xmm0, xmm0, 1
+vbroadcastsd ymm4, xmm0
+vextractf128 xmm0, ymm0, 0
+vbroadcastsd ymm7, xmm0
+shufpd xmm0, xmm0, 1
+vbroadcastsd ymm6, xmm0
+vmulpd ymm7, ymm3, ymm7
+vfmadd213pd ymm6, ymm2, ymm7
+vfmadd213pd ymm5, ymm1, ymm6
+vfmadd213pd ymm4, ymm0, ymm5
+vmovapd ymm8, ymm4\n".into(),
             highlight: 0,
             highlight_str: "0".into(),
         }
@@ -29,7 +38,7 @@ impl eframe::App for MyApp {
                 .with_rows(12)
                 .with_fontsize(14.0)
                 .with_theme(ColorTheme::GRUVBOX)
-                .with_syntax(Syntax::rust())
+                .with_syntax(Syntax::asm())
                 .with_numlines(true)
                 .show(ui, &mut self.code, &mut self.highlight);
             ui.text_edit_singleline(&mut self.highlight_str);
